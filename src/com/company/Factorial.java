fe4191b277f152db4a464b6ed82b40217f1cec43
Nathan <nathanyoung@billericak12.com>
2019-12-06 11:29:42 -0500

    saquon gasley

--- src/com/company/Factorial.java
+++ src/com/company/Factorial.java
@@ -1,4 +1,20 @@
 package com.company;
 
 public class Factorial {
+
+    public static long calcFactorial(int a){
+        long total=1;
+        while(a>0){
+            total=total*a;
+            a--;
+        }
+        return total;
+    }
+    
+
+    public static void main(String [] args){
+        for(int f=1; f<=20; f++){
+            System.out.println(calcFactorial(f));
+        }
+    }
 }
